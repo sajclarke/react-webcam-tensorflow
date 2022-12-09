40409c9e11af4fba102606ca8a6901e5bf2640ce
sajclarke <shannonajclarke@gmail.com>
2022-12-09 11:51:47 -0400

    chore: configure webcam

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Webcam from "react-webcam";
+//TODO: Import tensorflow and coco-ssd
+//TODO: Import drawing utility
 
 function App() {
+  const webcamRef = React.useRef(null);
+
+  React.useEffect(() => {
+    //TODO: Load the model
+    //TODO: Detect objects from the camera stream
+    //TODO: Display predictions in overlay
+  }, []);
+
   return (
     <div>
       <p>GDG Example</p>
-      <Webcam />
+      <Webcam
+        ref={webcamRef}
+        audio={false}
+        imageSmoothing={true}
+        videoConstraints={{
+          facingMode: "user",
+        }}
+      />
     </div>
   );
 }
